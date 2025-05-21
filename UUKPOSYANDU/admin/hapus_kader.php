a6d3f941c56b83fd18d50db062b53457658dc6e2
ItsMeAGS <160562717+ANANDAGUSTI@users.noreply.github.com>
2025-05-22 07:47:01 +0800

    Delete UUKPOSYANDU directory

--- UUKPOSYANDU/admin/hapus_kader.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../db/koneksi.php';
-$id_kader = $_GET['id'];
-$query = "DELETE FROM kader WHERE id_kader = $id_kader";
-mysqli_query($conn, $query);
-header("Location: index.php");
-?>
--- UUKPOSYANDU/kader/hapus_bayi.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../db/koneksi.php';
-$id_bayi = $_GET['id'];
-$query = "DELETE FROM bayi WHERE id_bayi = $id_bayi";
-mysqli_query($conn, $query);
-header("Location: index.php");
-?>
